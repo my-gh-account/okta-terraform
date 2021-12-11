4658e121d03fc3682d14e123c8e3ca8ee9bcd70b
Patrick Putman <patrick@teramind.co>
2021-12-11 23:56:31 +0000

    set up custom s3 bucket for backend

--- aws.tf
+++ aws.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
+  bucket = "terraform-okta-backend-pputman"
 
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
