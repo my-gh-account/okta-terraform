c85e49b40d4a76ca834c151fb1da93af32983db0
Patrick Putman <patrick@teramind.co>
2021-12-12 00:01:16 +0000

    added dynamodb to the aws configuration for the backend locking

--- aws.tf
+++ aws.tf
@@ -25,3 +25,14 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-okta-backend-pputman"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
