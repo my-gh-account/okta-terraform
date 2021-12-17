eb4fd659a03825c6f6b702b1d6555ac93a740819
Parick Putman <patrick@teramind.co>
2021-12-17 13:57:44 -0600

    changed main.tf

--- staging/services/main.tf
+++ staging/services/main.tf
@@ -23,11 +23,19 @@ resource "aws_security_group" "deployment_server_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "ssh-key" {
   key_name   = "ssh-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCcjRmSe6GElaE/Vlc561s+T/qAn8Y1QCLgHHOi8d0e7PRKMkXOF4cEtqtlRddVFnTLKcw7NmEcSh+O2gye6OwU2aT99UwRZXt7aOcozT8pV66zPFGI92/IN2VAPWTJsfmDe/g/8U0lWX41oeBEIMwuzZsb3TKKxcKPsXf301k6GlzNMUV7H+6sTwr/d0F/+oTtWNIr8yUjpPDIGT7/YTDSfhARbERLFBBoN1RpVI1K4JZ5ucJEKxVa7n3jZykcQUwO2d3HiJddmgNPSNce7daegFxgm7eT3jRazX/7wm8t3co1yANbhi7KZukR5/HnDQL2AvOPCR7vdd9wgBqEZrQz root@teraappliance9"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFnY0vRotC1QlHE/TAMNE7+Br1JcU/lxftN/nshuT5sZHQP6Iip6bm1dwxSLPulEcijhemyKwSDWq2bh672iIDPHbC/pZ+PFa/7U6AeJTyq7dTyrZia0DEl/hHsAudjxhmhfksOL1vqayP9wmnhpf0mnShkEHhvCSNjjJN3z3n9KmmW5ME+Fa8HYgAoFaHiWhz66HwwYqrskYysuS3RjPEbaWNA8VFqWIahVQueRdCSrn5SznMkNGmRW9iYeMjPQIsjqmSX+nrsOKfR6nOOrPyQ2Pf43FRjT+XLGYkZak/NyBciBO3CSQ8g58O8h0mAsELvaQk1eNNWakYHHtlfLDqw1X2LzNILEqr6nicJpTSjSMMSdUFFP5q7OlOmZoBlLz4jTfsKyM2ciyaE0YO5qXXst5M3h8uiEZfF5b32vZIW43hMEyvCUeQRnhLD8l0uhExT4IWSkg2MHC0rXMIy3tBI4aH2mC7HkHUDvvoeQFB1b7H/Fj2KUL3tjX9MfHRB65nT1mp3fT+aqMf6xSrn+LPa9dMTxXxccOZ+ly8NLsLEZ7xyuuXDxeLlT9p9dASQpPMhHzcOtsuEpuONFtaT4fq74vv4RRJISgrdwFcTfJ2hzv3ILPt5pYBG6VY27inWt8IzNQhx8ineBO9muqygKqvbbpyToSo2S1iBQtG4lSI4w== patrick@teramind.co"
+   
 }
 
 output "instance_ip" {
