34a1fe0bdeb6b2ce4752139d3f44235dea4e5934
Parick Putman <patrick@teramind.co>
2021-12-20 21:46:13 -0600

    added slack app

--- staging/accounts/okta-groups/main.tf
+++ staging/accounts/okta-groups/main.tf
@@ -76,11 +76,14 @@ module "okta-groups" {
     "aws-test-FullAccess"   = { rule = "user.email == \"putman.patrick@gmail.com\"" },
     "aws-975678609170-AdministratorAccess" = { rule = join(" ", [ # This join gives us a better way to specify larger, more complex rules on multiple lines.
       "user.email == \"putman.patrick@gmail.com\" OR",            # Admin
-      "user.email == \"sally@example.com\"        OR",            # CTO      
-      "user.email == \"bob@example.com\""
+#      "user.email == \"sally@example.com\"        OR",            # CTO      
+#      "user.email == \"bob@example.com\"          OR",
+      "user.email == \"test3@gmail.com\"" 
       ])
     },
     "aws-975678609170-test_policy2" = { rule = "user.email == \"putman.patrick@gmail.com\"" },
     "aws-975678609170-test_policy"  = { rule = "user.email == \"putman.patrick@gmail.com\"" },
+    "slack-regular_user"            = { rule = "user.email == \"putman.patrick@gmail.com\"" },
+    "slack-deserthomescleaning"              = { rule = "user.email == \"putman.patrick@gmail.com\"" },
   }
 }
---
+++ staging/accounts/slack-app/main.tf
@@ -0,0 +1,80 @@
+#-------------------------------------------------------------------------------------------------------------------------------------
+# OKTA S3 BACKEND
+# There's a number of reasons to use a backend instead of a local state, this is to use the specified key in s3 backend
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  backend "s3" {
+    key = "staging/accounts/slack-app/terraform.tfstate"
+  }
+}
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+# OKTA PROVIDER VERSION REQUIREMENTS 
+# Okta's resource requires you specify this version to work
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_providers {
+    slack = {
+      source  = "pablovarela/slack"
+    }
+    okta = {
+      source  = "okta/okta"
+    #   version = "~> 3.20"
+    }
+
+  }
+}
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+# VAULT VARIABLES 
+# Refers to variables for Hashicorp Vault in variables.tf
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+provider "vault" {
+  address = var.vault_address
+}
+
+data "vault_generic_secret" "slack_creds" {
+  path = var.vault_slack_secret_path
+}
+
+data "vault_generic_secret" "okta_creds" {
+  path = var.vault_okta_secret_path
+}
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+# OKTA CREDENTIALS
+# allows login to okta, api_token pointing here to data source created for hashicorp vault secure secret storage
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+provider "okta" {
+  org_name  = "teramindpputman"
+  base_url  = "okta.com"
+  api_token = data.vault_generic_secret.okta_creds.data["api_token"]
+}
+
+provider "slack" {
+  token = data.vault_generic_secret.slack_creds.data["api_token"]
+
+}
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+# MODULE REFERENCE
+# sets variables defined in module and can set in variables.tf, see module at source for explanation 
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+module "slack-app" {
+  source        = "../../../modules/accounts/slack-app/"
+}
+
+output "slack_groups" {
+  value     = module.slack-app.slack_groups
+}
+
